cf812ae4366bfb33cea9c336bd907693fe103ad8
Calum Medlock <calum.medlock@calmandesign.co.uk>
2023-01-06 15:45:24 +0000

    fixed the error with  variable

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ var totalChange = 0;
 for (var i = 0; i < finances.length; i++) {
   // Calculate the change in Profit/Losses from the previous month (if not the first month)
   if (i > 0) {
-    var change = finances[i][1] - finances[i+1][1];
+    var change = finances[i][1] - finances[i-1][1];
     totalChange += change;
   }
 }
